8f9d7984b554e48c7d2098db0f9d63e132e20837
zero <undefinedbytes@gmail.com>
2017-11-29 16:03:04 -0500

    Ajout classe client

--- UI/Modelisation/src/ui_projet/Magasin.java
+++ UI/Modelisation/src/ui_projet/Magasin.java
@@ -1,13 +1,24 @@
 package ui_projet;
 
+import java.util.ArrayList;
+
 public final class Magasin {
 
+	ArrayList<Client> listeClients = new ArrayList<Client>();
+	
+	
+	
 	private Magasin() {
 
 	}
 
+	
+	
+	
+	
 	public static void main(String[] args) {
-
+		
+		UI ui = new UI();
 	}
 
 }
